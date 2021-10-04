ff6a1ad07852ab16b9653ccedb97afb9c5bc06ac
thinhphan2002 <91036839+thinhphan2002@users.noreply.github.com>
2021-10-04 07:23:31 +0700

    Delete src/vn/edu/giadinh directory

--- src/vn/edu/giadinh/Program.java
+++
@@ -1,7 +0,0 @@
-package vn.edu.giadinh;
-
-public class Program {
-    public static void main(String[] args) {
-        System.out.println("Hi Program");
-    }
-}
--- src/vn/edu/giadinh/tuan04/baitapthem.java
+++
@@ -1,8 +0,0 @@
-package vn.edu.giadinh.tuan04;
-
-public class Vandanglambaitaptuan04 {
-    public static void main(String[] args) {
-        System.out.println("Đang làm bài cho tuần 4 thầy xem được thông cảm chưa nộp kịp");
-        
-    }
-}
